9a6806848e75c79a5a8be46a4dfbefa607f1ac50
Adi Abdulahovic <adiabdulahovic@icloud.com>
2021-09-16 11:41:19 +0000

    Edited the webserverscript.sh, removed the -P 6102 because we only enabled the default port 22, also changed the IP on it because we also only enabled SCP locally

--- scripts/webserverscript.sh
+++ scripts/webserverscript.sh
@@ -2,9 +2,9 @@
 
 timestamp=`date +%Y-%m-%d_%H:%M`;
 
-scp -P 6102 -i /home/adi91/.ssh/id_rsa adi91@haest.zapto.org:/var/log/apache2/access.log /home/adi91/backup/webserverlogs/access-$timestamp.log
-scp -P 6102 -i /home/adi91/.ssh/id_rsa adi91@haest.zapto.org:/var/log/apache2/error.log /home/adi91/backup/webserverlogs/error-$timestamp.log
-scp -P 6102 -i /home/adi91/.ssh/id_rsa adi91@haest.zapto.org:/var/log/auth.log /home/adi91/backup/webserverlogs/auth-$timestamp.log
-scp -P 6102 -i /home/adi91/.ssh/id_rsa adi91@haest.zapto.org:/var/log/faillog /home/adi91/backup/webserverlogs/faillog-$timestamp
+scp -i /home/adi91/.ssh/id_rsa adi91@192.168.1.25:/var/log/apache2/access.log /home/adi91/backup/webserverlogs/access-$timestamp.log
+scp -i /home/adi91/.ssh/id_rsa adi91@192.168.1.25:/var/log/apache2/error.log /home/adi91/backup/webserverlogs/error-$timestamp.log
+scp -i /home/adi91/.ssh/id_rsa adi91@192.168.1.25:/var/log/auth.log /home/adi91/backup/webserverlogs/auth-$timestamp.log
+scp -i /home/adi91/.ssh/id_rsa adi91@192.168.1.25:/var/log/faillog /home/adi91/backup/webserverlogs/faillog-$timestamp
 
 find /home/adi91/backup/webserverlogs/* -mtime +7 -exec rm {} \;
